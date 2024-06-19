406dcd622d9094ec27334dc69d1dadf4324f40be
Colby Beach <clawplusstacker@gmail.com>
2024-06-19 19:23:55 -0400

    Added API

--- client/App.js
+++ client/App.js
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Lets go return stuff</Text>
+      <Text>
+        Closify
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
